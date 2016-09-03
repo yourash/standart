e3a6f62d8aa654d2d8f7b905023cd4ba164e273e
yourash <y.anton.ua@gmail.com>
2016-09-03 17:13:06 +0300

    init

---
+++ commands.sh
@@ -0,0 +1,21 @@
+sudo add-apt-repository ppa:nrbrtx/sysvinit-backlight
+sudo apt-get update
+sudo apt-get install sysvinit-backlight
+
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get update; sudo apt-get install -y sublime-text-installer
+
+sudo apt install python3-pip
+sudo pip3 install Flask
+
+sudo apt-get install ubuntu-restricted-extras
+
+sudo apt-get install vim
+
+sudo apt-get install unity-tweak-tool
+
+sudo apt-get install git
+
+sudo update-grub
+
+
